fbbd79b4c6c4348e494b1e0ede815bf1c1bfcd71
Ugo Loobuyck <ugo813@hotmail.fr>
2020-04-19 23:13:39 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Automated generation of new `The Office` scripts using deep neural networks',
+    author='Ugo Loobuyck',
+    license='MIT',
+)
